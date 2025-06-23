a53754935d5947d2924ba76b67f1ab5c2aee8c33
Anshu Shah <infoanshu99@gmail.com>
2025-06-23 21:33:35 +0545

    list-tupple and dist

---
+++ list_tupple_dis/anshu.py
@@ -0,0 +1,30 @@
+a = 45
+print (a)
+name = "anshh shah"
+print("name")
+
+# print( a + "name")
+a1=30
+b1=45
+print(a1 +b1 )
+x= complex( 3,6)
+y= True
+z= "anshu"
+x1=12
+print(x)
+print(" the type of x:", type(x))
+print(" the type of y:", type(y))
+print(" the type of z:", type(z))
+print(" the type of x1:", type(x1))
+
+# list and the tupples 
+anshu=["anshu ","kripa",[2,3,4]]
+print(anshu)
+
+# tupples 
+wayne=(('anshu','aman ','zun'),2,4,5)
+print(wayne)
+
+#dis.
+student1={"name":"kripa","age":21,"current_address":"gaighat udayput"}
+print(student1)
