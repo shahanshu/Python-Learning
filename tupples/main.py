729ea00b08276846e80154301aa57e7eab4264f6
Anshu Shah <infoanshu99@gmail.com>
2025-06-25 21:18:29 +0545

    tupple & tupple methods

---
+++ tupples/main.py
@@ -0,0 +1,16 @@
+anshu=(5,65,6,76,"anshu") #single element is regarded as init
+
+#tupple are unchangable 
+
+print(type(anshu))
+print(anshu)
+
+
+#tupple is kinda constant list that doesn't change
+print("lenght of anshu tupple",len(anshu))
+print(anshu[3])
+if 24 in anshu:
+    print("yes prresent")
+else:
+    print("not present")
+
