474df1132f9f08f9f556df84867bb04764e69d9c
Anshu Shah <infoanshu99@gmail.com>
2025-06-25 18:05:58 +0545

    argument_types

---
+++ function_arguments/default.py
@@ -0,0 +1,5 @@
+def avg(a=5,b=7):
+    print(" the avearge of",a,b,":",(a+b)/2)
+
+# avg(4,5)
+avg(10) #default argument such that it accepts if provinding and ig not it takes from it's own default
---
+++ function_arguments/defaultstrings.py
@@ -0,0 +1,6 @@
+def info(fname="anshu",lname="shah",mname="kumar"):
+    print("Hellow",fname,"""so your surenae is 
+          """,lname,"may i call you as ",mname);
+info("kripa",mname="khatiwada") # explictly defines the middle name as well for the skipping some of the names 
+
+#keyword argument doesn't care about the order 
