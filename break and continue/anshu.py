be6fffab5922984a6369919cfeb7506b60802135
Anshu Shah <infoanshu99@gmail.com>
2025-06-25 17:09:44 +0545

    break & continue done

---
+++ break and continue/anshu.py
@@ -0,0 +1,10 @@
+# at the perticular iteration we wanted to either skip the loop or break the loop 
+# the loop works iteration by the iteration 
+for i in range(15):
+    if i ==11:
+        print("skip the iternation")
+        continue
+    print(i*5)
+
+# break means to exit the loop 
+#continue means to exit the iteration 
