8c34b1ca1fa9a483dab0a7de43cc8c2a2c47d6de
Anshu Shah <infoanshu99@gmail.com>
2025-06-25 20:23:11 +0545

    list_intrroduction done

---
+++ list/intoduction.py
@@ -0,0 +1,24 @@
+marks=[3,5,67,"anshu shah"]
+# print(marks[3])
+# print(marks)
+# print(marks[-2])  #Negative indexing
+# # print(len(marks))
+# if 5 in marks:
+#  print("yes")
+# else:
+#  print("no")
+
+
+# if "shah" in "anshu shah":
+#   print("yes")
+# else:
+#   print("no")
+# marks=[1,2,3,4,5,6,7]
+# print(marks[3:7])
+# print(marks[3:7:2])# this is the jump indexing first is always counted and then the jumping starts 
+
+#list comprehensive
+list=[i for i in range(10)]
+print(list)
+lst=[i*i for i in range(11) if (i%2==0)] # further we can add the conditions as well as the the expression in the list as well 
+print(lst)
