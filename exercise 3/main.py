f1f9bae5c79dc4d7caff10bcda8b0098c9800db2
Anshu Shah <infoanshu99@gmail.com>
2025-06-26 14:45:48 +0545

    done 1 exercise

---
+++ exercise 3/main.py
@@ -0,0 +1,19 @@
+"""import time
+while True:
+    num1=int(input("enter a number: "))
+
+    num2=int(input("enter a second number: "))
+    sum = num1 + num2
+    print("the sum is",sum)
+    choice=int(input("enter 0 to continue 1 for exit the program :"))
+    if choice ==1:
+        time.sleep(1.5)
+        print("thanks for playing !")
+        break
+    elif choice==0:
+        continue
+    else:
+        print("Invalid Entry")
+"""
+
+#prrogram to store the unser input in list ,tupple and dict 
