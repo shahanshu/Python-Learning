a4bb4c2a0f8fdbd227b44a94c8967c660e2a75ef
Anshu Shah <infoanshu99@gmail.com>
2025-06-27 12:01:18 +0545

    enumerate

---
+++ enumerable functions/main.py
@@ -0,0 +1,17 @@
+'''
+marks=[22,34,56,23,45,44,56]
+for anshu , i in enumerate( marks):
+    print(i)
+    if (anshu==3):
+        print("passed")
+'''
+'''
+scores=[3,4,5,7,3,8,3,2]
+for index, value in enumerate( scores):
+    print(f"index-{index} data-{value}")
+'''
+rank=[3,5,2,3,4,6]
+for index,ranks in enumerate(rank,start=1):
+    print(index,rank)
+    if index==2:
+        print("good job,anshu shah")
