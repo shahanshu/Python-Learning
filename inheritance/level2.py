3f92f9281e623935e65db674b2d2e0eecb595edb
Anshu Shah <infoanshu99@gmail.com>
2025-06-29 12:42:58 +0545

    access MOdifiers

--- inheritance/level2.py
+++ inheritance/level2.py
@@ -6,6 +6,16 @@
 
 class studets:
     def __init__(self):
-        self.name="Anshu Shah"
+        self.__name="Anshu Shah"
 s1=studets()
-print(s1.name)
+print(s1._studets__name) 
+"""
+can't be accessed directly but can be indirectly
+"""
+
+try:
+ print(s1.name)
+except Exception as f:
+ print(f"errror as: { f}")  # thows error 
+print(s1.__dir__())
+""" availabe methods on the s1"""
