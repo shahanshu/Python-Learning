bf4c282e2930affb6082e013ceee0be945f8e92e
Anshu Shah <infoanshu99@gmail.com>
2025-07-13 13:29:03 +0545

    hybrid _&_ hiereachicxcal inheritance

---
+++ OOP-hybrid-hiereachical-inheritance/eg1.py
@@ -0,0 +1,32 @@
+'''
+
+Hybrid Inheritance Example
+class baseClass:
+    pass
+
+
+
+class derived1(baseClass):
+    pass
+
+
+class derived2(baseClass):
+    pass
+
+
+class derived3(derived1,derived2):
+    pass
+
+'''
+class baseClass:
+    pass
+
+
+class D1(baseClass):
+    pass
+
+class D2(baseClass):
+    pass
+
+class D3(baseClass):
+    pass
